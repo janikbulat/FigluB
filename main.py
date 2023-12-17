1c8d4b68cace6432290798f94d5eaa6fb697fc9a
janikbulat <janbulat789@gmail.com>
2023-12-17 19:58:01 +0300

    class of FGWindow

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 import pygame
+
+class FGWindow:
+    def __init__(self):
+        self.window = pygame.display.set_mode((900, 700))
+        pygame.display.set_caption('FigluB')
+
+    def start(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+        pygame.quit()
+
+figlub_window = FGWindow()
+figlub_window.start()
